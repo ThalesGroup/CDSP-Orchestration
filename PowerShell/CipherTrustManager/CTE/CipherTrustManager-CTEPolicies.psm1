01682e42fca8997ae68f862d35a49557e58062c2
anugram <anurag.jain@thalesgroup.com>
2023-08-31 17:58:11 -0400

    Update CipherTrustManager-CTEPolicies.psm1

--- PowerShell/CipherTrustManager/CTE/CipherTrustManager-CTEPolicies.psm1
+++ PowerShell/CipherTrustManager/CTE/CipherTrustManager-CTEPolicies.psm1
@@ -381,6 +381,122 @@ function New-CTEPolicyLDTKeyRulesList {
     return $ldtKeyRulesList
 }
 
+function New-CTEPolicySecurityRulesList {
+    param(
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true)]
+        [AllowEmptyCollection()]
+        [hashtable[]]$securityRulesList,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [string] $action,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [string] $effect,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [string] $process_set_id,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [string] $resource_set_id,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [string] $user_set_id,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [bool] $exclude_process_set,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [bool] $exclude_resource_set,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [bool] $exclude_user_set,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [bool] $partial_match
+    )
+    Write-Debug "Start: $($MyInvocation.MyCommand.Name)"
+
+    if ($securityRulesList) {
+        Write-Debug "array: $($securityRulesList)"
+    }
+    else {
+        Write-Debug "array is empty"
+        $securityRulesList = @()
+    }
+
+    $temp_hash = @{}
+    
+    if ($action) {
+        $temp_hash.add('action', $action)
+    }
+    if ($effect) {
+        $temp_hash.add('effect', $effect)
+    }
+    if ($exclude_process_set -ne $null) {
+        $temp_hash.add('exclude_process_set', $exclude_process_set)
+    }
+    if ($exclude_resource_set -ne $null) {
+        $temp_hash.add('exclude_resource_set', $exclude_resource_set)
+    }
+    if ($exclude_user_set -ne $null) {
+        $temp_hash.add('exclude_user_set', $exclude_user_set)
+    }
+    if ($partial_match -ne $null) {
+        $temp_hash.add('partial_match', $partial_match)
+    }
+    if ($resource_set_id) {
+        $temp_hash.add('resource_set_id', $resource_set_id)
+    } 
+    if ($process_set_id) {
+        $temp_hash.add('process_set_id', $process_set_id)
+    }
+    if ($user_set_id) {
+        $temp_hash.add('user_set_id', $user_set_id)
+    }    
+
+    #Add this current policy to the list of user set policies
+    $securityRulesList += $temp_hash
+    Write-Debug "array updated: $($securityRulesList)"
+
+    Write-Debug "End: $($MyInvocation.MyCommand.Name)"
+    return $securityRulesList
+}
+
+function New-CTEPolicySignatureRulesList {
+    param(
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true)]
+        [AllowEmptyCollection()]
+        [hashtable[]]$signatureRulesList,
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true )]
+        [string] $signature_set_id
+    )
+    Write-Debug "Start: $($MyInvocation.MyCommand.Name)"
+
+    if ($signatureRulesList) {
+        Write-Debug "array: $($signatureRulesList)"
+    }
+    else {
+        Write-Debug "array is empty"
+        $signatureRulesList = @()
+    }
+
+    $temp_hash = @{}
+    
+    if ($signature_set_id) {
+        $temp_hash.add('signature_set_id', $signature_set_id)
+    }
+
+    #Add this current policy to the list of user set policies
+    $signatureRulesList += $temp_hash
+    Write-Debug "array updated: $($signatureRulesList)"
+
+    Write-Debug "End: $($MyInvocation.MyCommand.Name)"
+    return $signatureRulesList
+}
+
 function New-CTELDTKey {
     param(
         [Parameter(Mandatory = $false,
