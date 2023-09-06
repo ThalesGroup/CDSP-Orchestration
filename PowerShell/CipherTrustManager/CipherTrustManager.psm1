586e1b328c036fa30ee5f4976a26aff9a1b19e18
anugram <anurag.jain@thalesgroup.com>
2023-09-06 11:14:55 -0400

    falling back to old version

--- PowerShell/CipherTrustManager/CipherTrustManager.psm1
+++ PowerShell/CipherTrustManager/CipherTrustManager.psm1
@@ -95,59 +95,11 @@ function Connect-CipherTrustManager {
     return
 }
 
-<#
-    .SYNOPSIS
-    Create a connection to CipherTrust Manager and store AuthToken for use in future calls
-
-    .DESCRIPTION
-    Create a connection to CipherTrust Manager and store AuthToken (JWT) for use in future calls. It uses Get-JWT to manage the lifecycle of your JWT token so it is `set and forget`
-
-    .PARAMETER server
-    Specifies the IP Address or FQDN of CipherTrust Manager.
-
-    .PARAMETER user
-    Specifies the username for the account authorized to connect with CipherTrust manager.
-
-    .PARAMETER pass
-    Specifies the password (in plaintext for now) for the user.
-
-    .INPUTS
-    None. You cannot pipe objects to Connect-CipherTrustManager.
-
-    .OUTPUTS
-    None. Connect-CipherTrustManager returns a proxy to this connection.
-
-    .EXAMPLE
-    PS> Connect-CipherTrustManager -server 10.23.104.40 -user "user1" -pass "P@ssw0rd!"
-
-    .LINK
-    Online version: https://github.com/thalescpl-io/CDSP_Orchestration/tree/main/PowerShell/CipherTrustManager
-#>
-
-function Disconnect-CipherTrustManager {
-    param()
-
-    Write-Debug "Start: $($MyInvocation.MyCommand.Name)"
-
-    $CM_Session.KMS_IP    = $null
-    $CM_Session.User      = $null
-    $CM_Session.Pass      = $null
-    $CM_Session.REST_URL  = $null
-    $CM_Session.AuthToken = $null
-
-    Write-Debug "Session Variables have been cleared"
-
-
-    Write-Debug "End: $($MyInvocation.MyCommand.Name)"
-    return
-}
-
 ###
 # Exports
 ###
 #This module
 Export-ModuleMember -Function Connect-CipherTrustManager
-Export-ModuleMember -Function Disconnect-CipherTrustManager
 #Utils
 Export-ModuleMember -Function Get-CMJWT
 Export-ModuleMember -Function Test-CMJWT
@@ -156,10 +108,6 @@ Export-ModuleMember -Function Write-HashtableArray
 Export-ModuleMember -Function Find-CMKeys
 Export-ModuleMember -Function New-CMKey
 Export-ModuleMember -Function Remove-CMKey
-#Newly Added for keys
-Export-ModuleMember -Function New-CMKeyMeta
-Export-ModuleMember -Function New-CMKeyMetaPermission
-Export-ModuleMember -Function New-CMKeyMetaCTEParams
 #Users
 Export-ModuleMember -Function Find-CMUsers
 Export-ModuleMember -Function New-CMUser
@@ -190,10 +138,6 @@ Export-ModuleMember -Function Find-CMAccessPolicies
 Export-ModuleMember -Function New-CMAccessPolicy
 Export-ModuleMember -Function Remove-CMAccessPolicy
 Export-ModuleMember -Function New-CMUserSetPolicy
-#Info
-Export-ModuleMember -Function Get-CMInfo
-Export-ModuleMember -Function Get-CMVersion
-Export-ModuleMember -Function Set-CMName
 #Interfaces
 Export-ModuleMember -Function Find-CMInterfaces
 Export-ModuleMember -Function New-CMInterface
@@ -208,49 +152,3 @@ Export-ModuleMember -Function New-CMDPGJSONRequestResponse
 Export-ModuleMember -Function Find-CMClientProfiles
 Export-ModuleMember -Function New-CMClientProfiles
 Export-ModuleMember -Function Remove-CMClientProfiles
-#CCKM
-Export-ModuleMember -Function New-CKSAWSParam
-Export-ModuleMember -Function New-CKSLocalHostedParam
-Export-ModuleMember -Function New-CKS
-Export-ModuleMember -Function Remove-CKS
-Export-ModuleMember -Function Edit-CKS
-Export-ModuleMember -Function Update-CKSPerformOperation
-#Syslog Connections
-Export-ModuleMember -Function Find-CMSyslogs
-Export-ModuleMember -Function New-CMSyslog
-Export-ModuleMember -Function Get-CMSyslog
-Export-ModuleMember -Function Remove-CMSyslog
-Export-ModuleMember -Function Set-CMSyslog 
-#CTE
-##PolicyElements
-Export-ModuleMember -Function New-CTEPolicyElement
-Export-ModuleMember -Function New-CTEElementsList
-Export-ModuleMember -Function Find-CTEPolicyElementsByType
-#Export-ModuleMember -Function Remove-CTEPolicyElement
-#Export-ModuleMember -Function Update-CTEPolicyElement
-#Export-ModuleMember -Function Update-CTEPolicyElementAddElements
-#Export-ModuleMember -Function Remove-CTEPolicyElementDeleteElements
-#Export-ModuleMember -Function Update-CTEPolicyElementUpdateElementByIndex
-##Policy
-Export-ModuleMember -Function New-CTEPolicy
-Export-ModuleMember -Function New-CTEPolicyDataTxRulesList
-Export-ModuleMember -Function New-CTEPolicyIDTKeyRulesList
-Export-ModuleMember -Function New-CTEPolicyKeyRulesList
-Export-ModuleMember -Function New-CTEPolicyLDTKeyRulesList
-Export-ModuleMember -Function New-CTEPolicySecurityRulesList
-Export-ModuleMember -Function New-CTEPolicySignatureRulesList
-Export-ModuleMember -Function New-CTELDTKey
-Export-ModuleMember -Function New-CTEPolicyMetadata
-##Client
-Export-ModuleMember -Function New-CTEPolicy
-Export-ModuleMember -Function Find-CMDPGPolicies
-Export-ModuleMember -Function Update-CTEClient
-Export-ModuleMember -Function New-CTEGuardPointParams
-Export-ModuleMember -Function New-CTEClientGuardPoint
-Export-ModuleMember -Function Find-CTEClientGuardPoints
-Export-ModuleMember -Function Remove-CTEClientGuardPoint
-Export-ModuleMember -Function New-CTECSIStorageGroup
-Export-ModuleMember -Function Find-CTECSIStorageGroups
-Export-ModuleMember -Function Remove-CTECSIStorageGroup
-Export-ModuleMember -Function New-CTEAddClientsStorageGroup
-Export-ModuleMember -Function New-CTEAddGuardPoliciesStorageGroup
